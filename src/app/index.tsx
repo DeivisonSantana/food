3d1be029bce4d96ce3ad169f2b86edd111f33bef
DeivisonSantana <deivisonssouza@hotmail.com>
2024-11-17 11:20:14 -0400

    add(components): Header * componente header adicionado e estilizado

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,13 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
+import { Header } from '@/src/components/header/index';
+import Constants from "expo-constants"
+
+const statusBarHeight = Constants.statusBarHeight
 
 export default function Index() {
   return (
-    <View className="flex justify-center items-center w-full h-full">
-      <Text className="text-blue-400">Hello Word!</Text>
-    </View>
+    <ScrollView style={{flex:1}} className="bg-slate-200" showsVerticalScrollIndicator={false}>
+      <View className="w-full px-4" style={{marginTop: statusBarHeight + 8}}><Header/></View>
+    </ScrollView>
   );
 }
