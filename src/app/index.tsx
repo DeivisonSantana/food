f9746fb3441e531a0b4087461e8a63d8528c34c4
DeivisonSantana <deivisonssouza@hotmail.com>
2024-11-17 10:52:03 -0400

    initial commit * add tailwind and remove example project

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex justify-center items-center w-full h-full">
+      <Text className="text-blue-400">Hello Word!</Text>
+    </View>
+  );
+}
