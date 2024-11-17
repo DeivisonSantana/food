0417fb3dc3d9129451707faad0da414bf80b3b58
DeivisonSantana <deivisonssouza@hotmail.com>
2024-11-17 11:48:53 -0400

    add(components): banners *  add banners e imagens do banner tbm foi instalado a lib pagerview

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,17 @@
 import { Text, View, ScrollView } from "react-native";
 import { Header } from '@/src/components/header/index';
 import Constants from "expo-constants"
+import { Banner } from "@/src/components/banner";
 
 const statusBarHeight = Constants.statusBarHeight
 
 export default function Index() {
   return (
     <ScrollView style={{flex:1}} className="bg-slate-200" showsVerticalScrollIndicator={false}>
-      <View className="w-full px-4" style={{marginTop: statusBarHeight + 8}}><Header/></View>
+      <View className="w-full px-4" style={{marginTop: statusBarHeight + 8}}>
+        <Header/>
+        <Banner/>
+        </View>
     </ScrollView>
   );
 }
